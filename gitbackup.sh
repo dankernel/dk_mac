dd1131824623ad479ac89e66171ac41a2945a28d
dkdk <dkdk@localhost.localdomain>
2015-06-16 12:52:20 +0900

    auto-20150616-125220

--- gitbackup.sh
+++ gitbackup.sh
@@ -2,7 +2,7 @@
 
 WORK=1
 # find -size +40M -not -path "./" | grep -q "\." && WORK=0
-find . ! \( -path './data' './out' -prune \) -size +40M -not -path "./" | grep "" && WORK=0
+# find . ! \( -path './data' './out' -prune \) -size +40M -not -path "./" | grep "" && WORK=0
 
 if [ $WORK = 1 ]; then
   echo -e '\E[34m'"\033[1m[O  K] 40M\033[0m"
